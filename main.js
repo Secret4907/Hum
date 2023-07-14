ccedca0e0075949225298213cfa7a92ae39e2f98
Secret4907 <113611720+Secret4907@users.noreply.github.com>
2023-07-14 13:30:34 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,80 @@
+x = 0;
+y = 0;
+
+screenWidth = 0;
+screenHeight = 0;
+
+drawApple = "";
+
+speakData = "";
+toNumber = "";
+
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function preload() {
+  drawApple = loadImage()
+}
+
+var SpeechRecognition = window.webkitSpeechRecognition
+
+var recognition = new SpeechRecognition();
+
+function start()
+{
+  document.getElementById("status").innerHTML = "O sistema está ouvindo. Por favor, fale.";  
+  recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+
+toNumber = Number(content)
+
+if(Number.isInteger(to_number)) {
+  document.getElementById("status").innerHTML = "A maçã começou a ser desenhada"
+  drawApple = "set";
+}
+else {
+  document.getElementById("status").innerHTML = "O número não foi reconhecido."
+}
+
+ console.log(event); 
+
+ content = event.results[0][0].transcript;
+
+    document.getElementById("status").innerHTML = "A fala foi reconhecida: " + content; 
+
+}
+
+function setup() {
+ screenWidth = window.innerWidth;
+ screenHeight = window.innerHeight;
+ createCanvas(screenWidth, screenHeight-150)
+ canvas.position(0,150)
+}
+
+function draw() {
+  if(drawApple == "set")
+  { 
+    for(var i = 1 ; i <= to_number; i++) {
+      x = Math.floor(Math.random() * 700)
+      y = Math.floor(Math.ramdon() * 400)
+      Image(applle, x, y, 50, 50)
+    }
+    document.getElementById("status").innerHTML = toNumber + " maçãs desenhadas";
+    speakData = to_number + "maçãs desenhadas"
+    speak()
+    drawApple = "";
+  }
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    var utterThis = new SpeechSynthesisUtterance(speakData);
+
+    synth.speak(utterThis);
+
+    speakData = "";
+}
